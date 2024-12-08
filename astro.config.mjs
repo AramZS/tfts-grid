cf4b7cc2e6d9960d3f7ccb44e3c205d8b907d522
Aram Zucker-Scharff <aramzs@hacktext.com>
2024-12-08 01:03:40 -0500

    Setting up new pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
 
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
 
-import partytown from '@astrojs/partytown';
+import partytown from "@astrojs/partytown";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), partytown(), sitemap()],
-  adapter: vercel()
+	site: "https://grid.talesfromthesp.in",
+	integrations: [mdx(), partytown(), sitemap()],
+	adapter: vercel(),
 });
